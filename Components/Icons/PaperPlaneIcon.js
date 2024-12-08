ae8d8a6549126a70e8fe65a2dc85ebfac08568a7
FERNANDO SABA ARBACHE <61833107+arbachegit@users.noreply.github.com>
2024-12-08 20:19:13 -0300

    V1.46

--- Components/Icons/PaperPlaneIcon.js
+++ Components/Icons/PaperPlaneIcon.js
@@ -4,24 +4,17 @@ import React from 'react';
 
 // Definir componente globalmente
 window.PaperPlaneIcon = function PaperPlaneIcon({ className }) {
-  return (
-    <svg
-      className={className || "paper-plane-icon"}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      style={{
-        width: '20px', 
-        height: '20px', 
-        marginLeft: '8px', 
-        stroke: 'currentColor',
-        transform: 'rotate(45deg)'
-      }}
-    >
-      <g>
-        <path d="M22 2L11 13" />
-        <path d="M22 2L15 22L11 13L2 9L22 2Z" />
-      </g>
-    </svg>
-  );
+  return React.createElement("svg", {
+    className: className || "paper-plane-icon",
+    viewBox: "0 0 24 24",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg",
+    style: {
+      width: '20px',
+      height: '20px',
+      marginLeft: '8px',
+      stroke: 'currentColor',
+      transform: 'rotate(45deg)'
+    }
+  }, /* ... resto do SVG ... */);
 }; 
